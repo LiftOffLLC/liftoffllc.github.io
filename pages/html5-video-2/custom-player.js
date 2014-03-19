39fec8f0c0a284fb67df742710c6afeb970b4987
Chandan Kumar <chandankumar.r@gmail.com>
2014-03-19 17:26:20 +0530

    add video 2 example

---
+++ pages/html5-video-2/custom-player.js
@@ -0,0 +1,13 @@
+var app = {}; // Sort of namespacing
+app.videoSrc = 'http://video-js.zencoder.com/oceans-clip.mp4';
+app.startTime = 5;
+app.endTime = 25;
+app.p1 = _V_('example_video_1');
+app.p1.src(app.videoSrc + '#t=' + app.startTime + ',' + app.endTime);
+app.restrictRange = function(){
+    if(app.p1.currentTime() > app.endTime || app.p1.currentTime() < app.startTime) {
+        app.p1.currentTime(app.startTime);
+    }
+}
+
+app.p1.on('seeked', app.restrictRange).on('play', app.restrictRange);
