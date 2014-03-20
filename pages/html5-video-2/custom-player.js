a0a8c45b4153734b065782041465a2097a5e3c24
Chandan Kumar <chandankumar.r@gmail.com>
2014-03-20 17:20:51 +0530

    custom play

--- pages/html5-video-2/custom-player.js
+++ pages/html5-video-2/custom-player.js
@@ -2,16 +2,8 @@ var app = {}; // Sort of namespacing
 app.videoSrc = 'http://video-js.zencoder.com/oceans-clip.mp4';
 app.startTime = 5;
 app.endTime = 25;
-app.p1 = _V_('example_video_1');
-app.p1.src(app.videoSrc + '#t=' + app.startTime + ',' + app.endTime);
-app.restrictRange = function(e){
-    if(app.p1.currentTime() > app.endTime || app.p1.currentTime() < app.startTime) {
-        app.p1.currentTime(app.startTime);
-        console.log(e);
-    }
-}
-
-app.p1.on('seeked', app.restrictRange)
-app.p1.on('playing', function(){
-  app.p1.on('timeupdate', app.restrictRange);
+if(!_V_){var _V_ = vjs}
+app.p1 = _V_('example_video_1', {range : {startTime:5, endTime: 25}}, function(){
+  var player = this;
+  player.rangePlay({startTime: 5, endTime: 25});
 });
