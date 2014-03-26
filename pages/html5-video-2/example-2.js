2bc4444a614f2e3c6d3b0f5da8e5ea21556acf02
Chandan Kumar <chandankumar.r@gmail.com>
2014-03-26 12:06:27 +0530

    minor reference fixes

--- pages/html5-video-2/example-2.js
+++ pages/html5-video-2/example-2.js
@@ -2,4 +2,7 @@ var app = {}; // Sort of namespacing
 app.videoSrc = 'http://video-js.zencoder.com/oceans-clip.mp4';
 app.startTime = 5;
 app.endTime = 25;
-app.p1 = _V_('example_video_1', {range : {startTime:5, endTime: 25}});
+app.p1 = _V_('example_video_1', {}, function(){
+  var player = this;
+  player.rangePlay({startTime: 5, endTime: 25});
+});
