9220f1ea08a5089978043eaa76296f5a17c986ff
Chandan Kumar <chandankumar.r@gmail.com>
2014-03-26 11:54:55 +0530

    add second example

---
+++ pages/html5-video-2/example-2.js
@@ -0,0 +1,5 @@
+var app = {}; // Sort of namespacing
+app.videoSrc = 'http://video-js.zencoder.com/oceans-clip.mp4';
+app.startTime = 5;
+app.endTime = 25;
+app.p1 = _V_('example_video_1', {range : {startTime:5, endTime: 25}});
